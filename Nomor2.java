4769973774c891773e2ac9a2b1d8fffa6bdd2628
viona <vdevaqaulika@gmail.com>
2024-10-24 14:48:07 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,37 @@
 public class Nomor2{
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner (System.in);
+
+        
+        System.out.print("masukkan nama produk [Labubu/Jenglot/Voodo]: ");
+        String pilihan = scanner.nextLine();
+
+        System.out.print("jumlah pembelian: ");
+        String Pilihan = scanner.nextLine();
+
+        if (pilihan.equals("labubu")) {
+            System.out.println(" harga= 300000;: ");
+                    System.out.println();
+                }
+
+        else if (pilihan.equals("jenglot")) {
+            System.out.println(" harga*= 250000 ;: ");
+                    System.out.println();
+                        }
+
+        else 
+            System.out.println(" harga*= 200000 ;: ");
+                 System.out.println();
+        }
+
+        
+
+
+    }
+
+        
+       
+
     }
 }
