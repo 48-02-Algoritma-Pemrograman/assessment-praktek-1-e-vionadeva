707e3a9e5499fda5cc6222ff940c06a7229c5ae7
viona <vdevaqaulika@gmail.com>
2024-10-24 14:26:44 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,32 @@
 public class Nomor1{
     public static void main(String[] args){
+       
+        Scanner input = new Scanner(System.in);
+
+        int tunjanganKesehatan;
+        int jumlahAnggotaKeluarga;
+        int jumlahTunjangan_per_jiwa = 100000;
+ 
+        System.out.print("jumlah anggota keluarga: ");
+        int jumlah = scanner.nextInt();
+
+        System.out.print("besar tunjangan kesehatan: ");
+        int tunjangan = scanner.nextInt();
+
+        if (jumlahAnggotaKeluarga <= 3) {
+            tunjanganKesehatan = 1* jumlahTunjangan_per_jiwa;
+        }  else if ( jumlahAnggotaKeluarga >= 3 && ); {
+            tunjanganKesehatan = 1 * jumlahTunjangan_per_jiwa;
+        }  else {
+            tunjanganKesehatan = 50000
+
+        }
+
+        System.out.println("Besar tunjangan kesehatan: Rp " + tunjanganKesehatan);
+
+        }
+    
+    }
 
     }
 }
